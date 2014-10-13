06d1143d74c780742d1b09c7657c934d13b8ee4a
Frank Allenby <fallenby@outlook.com>
2014-10-13 16:45:38 +0200

    Slider functionality added, some cleanup

--- js/slider.js
+++ js/slider.js
@@ -1,69 +1,157 @@
-(function(slideDuration) {
-  var element;
-  var currentImage = 0;
-  var images;
-  var opacity = {
-    current: 0.4,
-    min: 0.4,
-    max: 1 
-  };
+var dotSlider;
 
-  var slideTimeout;
-  var slideSpeed = 40;
+(dotSlider = function(slideDuration, slideInterval) {
+    /*
+     * Public interface
+     */
 
-  window.addEventListener('load', function() {
-    element = document.getElementsByClassName('wrap')[0];
-    images = document.body.attributes['slides'].value.split(',');
+    dotSlider.add = addItem;
+    dotSlider.remove = removeItem;
 
-    setBackground(images[0]);
 
-    if (images.length > 1)
-        setInterval(slideIn, 5000);
-  });
+    /*
+     * Private interface
+     */
 
-  function slideIn()
-  {
-    opacity.current += 0.1;
+    var element;
+    var currentImage = 0;
+    var images;
+    var active = false;
+    var opacity = {
+        current: 0.4,
+        min: 0.4,
+        max: 1 
+    };
 
-    updateBackground();
+    var slideInterval;
+    var slideTimeout;
+    var slideSpeed = 40;
 
-    if (opacity.current < opacity.max)
-      slideTimeout = setTimeout(slideIn, slideSpeed);
-    else
+    window.addEventListener('load', function() {
+        element = document.getElementsByClassName('wrap')[0];
+        images = document.body.attributes['slides'].value.split(',');
+
+        if (!images ||a images[0] == '')
+            images = [];
+
+        document.body.style.backgroundSize = 'cover';
+
+        setBackground(images[0]);
+
+        if (images.length > 1)
+            startSlide();
+    });
+
+    function startSlide()
     {
-      updateImage();
-      slideOut();
+        if (active)
+            return;
+
+        slideInterval = setInterval(slideIn, slideInterval);
+        active = true;
     }
-  }
 
-  function slideOut()
-  {
-    opacity.current -= 0.1;
+    function stopSlide()
+    {
+        if (!active)
+            return;
 
-    updateBackground();
+        if (slideInterval)
+            clearInterval(slideInterval);
 
-    if (opacity.current > opacity.min)
-      slideTimeout = setTimeout(slideOut, slideSpeed);
-  }
+        if (slideTimeout)
+            clearTimeout(slideTimeout);
 
-  function updateBackground()
-  {
-    element.style.background = 'rgba(255, 255, 255, ' + opacity.current + ')';
-  }
-  
-  function updateImage()
-  {
-    if (currentImage == (images.length - 1))
-      currentImage = 0;
-    else
-      currentImage += 1;
+        active = false;
+    }
 
-    setBackground(images[currentImage]);
-  }
+    function slideIn()
+    {
+        opacity.current += 0.1;
 
-  function setBackground(filename)
-  {
-    document.body.style.background = 'url(img/slides/' + filename + ') no-repeat 0 0';
-    document.body.style.backgroundSize = 'cover';
-  }
-})(1000);
+        updateBackground();
+
+        if (opacity.current < opacity.max)
+            slideTimeout = setTimeout(slideIn, slideSpeed);
+        else
+        {
+            updateImage();
+            slideOut();
+        }
+    }
+
+    function slideOut()
+    {
+        opacity.current -= 0.1;
+
+        updateBackground();
+
+        if (opacity.current > opacity.min)
+            slideTimeout = setTimeout(slideOut, slideSpeed);
+    }
+
+    function updateBackground()
+    {
+        element.style.background = "rgba(255, 255, 255, " + opacity.current + ")";
+    }
+
+    function updateImage()
+    {
+        if (currentImage == (images.length - 1))
+            currentImage = 0;
+        else
+            currentImage += 1;
+
+        setBackground(images[currentImage]);
+    }
+
+    function setBackground(filename)
+    {
+        if (document.body.style.background == 'url(img/slides/' + filename + ') no-repeat 0 0')
+            return false;
+       
+        if (!filename)
+            document.body.style.background = '';
+        else 
+            document.body.style.background = 'url(img/slides/' + filename + ') no-repeat 0 0';
+
+        return true;
+    }
+
+    function addItem(uri)
+    {
+        for (var i = 0; i < images.length; ++i)
+            if (images[i] == uri)
+                return false;
+
+        images.push(uri);
+
+        if (images.length == 1)
+            setBackground(uri);
+        else
+            startSlide();
+
+        return true;
+    }
+
+    function removeItem(uri)
+    {
+        for (var i = 0; i < images.length; ++i)
+        {
+            if (images[i] == uri)
+            {
+                images.splice(i, 1);
+                
+                if (images.length == 0)
+                {
+                    setBackground(null);
+                    stopSlide();
+                }
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+})(1000, 5000);
